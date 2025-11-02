8e1e4b70661072f6727b0edf4edac2174bd89af2
Bruna Lara <brunalara28@hotmail.com>
2025-11-02 16:40:58 -0300

    Trabalho Cypress Final

--- cypress/e2e/automation-exercise.cy.js
+++ cypress/e2e/automation-exercise.cy.js
@@ -1,36 +1,199 @@
-describe('Automation Exercise', () => {
-    it('Login de Usuário com e-mail e senha corretos', () => {
-        cy.visit('https://automationexercise.com/')
-        cy.get('a[href="/login"]').click()
+/// <reference types="cypress" />
 
-        cy.get('[data-qa="login-email"]').type('qa-tester-1759530219181@test.com')
-        cy.get('[data-qa="login-password"]').type('12345')
+let userData;
+let userEmail;
+const userName = 'QABL5';
 
-        cy.get('[data-qa="login-button"]').click()
+describe('Automation Exercise - Testes Completos', () => {
+  let userData;
+  let userEmail;
+  const userName = 'QABL5';
 
-        const nomeDoUsuario = "QA Tester"
+  before(() => {
+    userEmail = `qabl-${new Date().getTime()}@test.com`;
+  });
 
-        cy.get('i.fa-user').parent().should('contain', nomeDoUsuario)
-        cy.get('a[href="/logout"]').should('be.visible')
-
-        cy.get(':nth-child(10) > a')
-            .should('be.visible')
-            .and('have.text', `Logged in as ${nomeDoUsuario}`);
-
-        cy.contains('b', nomeDoUsuario)
-        cy.contains(`Logged in as ${nomeDoUsuario}`).should('be.visible')
-        cy.contains(`Logged in as ${nomeDoUsuario}`).should('be.visible')
+  beforeEach(() => {    
+    cy.viewport('iphone-xr');
+    cy.visit('https://automationexercise.com');
+    cy.fixture('userData').then((data) => {
+      userData = data;
     });
+  });
 
-    it('Login de Usuário com e-mail e senha incorretos', () => {
-        cy.visit('https://automationexercise.com/')
-        cy.get('a[href="/login"]').click()
+  it('Registrar Usuário', () => {
+    cy.get('a[href="/login"]').click();
+    cy.get('[data-qa="signup-name"]').type(userName);
+    cy.get('[data-qa="signup-email"]').type(userEmail);
+    cy.contains('button', 'Signup').click();
 
-        cy.get('[data-qa="login-email"]').type('qa-tester-1759530219181@test.com')
-        cy.get('[data-qa="login-password"]').type('54321')
+    cy.get('[data-qa="password"]').type('12345');
+    cy.get('select[data-qa=days]').select('20');
+    cy.get('select[data-qa=months]').select('September');
+    cy.get('select[data-qa=years]').select('1992');
+    cy.get('input#newsletter').check();
+    cy.get('input#optin').check();
+    cy.get('input#first_name').type('Breno');
+    cy.get('input#last_name').type('Bruno');
+    cy.get('input#company').type('AMZ');
+    cy.get('input#address1').type('RUA 1');
+    cy.get('select#country').select('Canada');
+    cy.get('input#state').type('California');
+    cy.get('input#city').type('Los Angeles');
+    cy.get('[data-qa=zipcode]').type('90001');
+    cy.get('[data-qa=mobile_number]').type('222 333 4444');
 
-        cy.get('[data-qa="login-button"]').click()
+    cy.get('[data-qa="create-account"]').click();
+    cy.contains('b', 'Account Created!').should('be.visible');
+  });
 
-        cy.get('.login-form > form > p').should('contain', 'Your email or password')
+  it('Login de Usuário com e-mail e senha corretos', () => {
+    cy.get('a[href="/login"]').click();
+    cy.get('[data-qa="login-email"]').type(userEmail); 
+    cy.get('[data-qa="login-password"]').type('12345');
+    cy.get('[data-qa="login-button"]').click();
+
+    cy.get('li > a')
+      .contains(`Logged in as ${userName}`, { timeout: 10000 })
+      .should('be.visible');
+
+    cy.get('a[href="/logout"]').should('be.visible');
+  });
+
+  it('Login de Usuário com e-mail e senha incorretos', () => {
+    cy.get('a[href="/login"]').click();
+    cy.get('[data-qa="login-email"]').type(userEmail);
+    cy.get('[data-qa="login-password"]').type('54321');
+    cy.get('[data-qa="login-button"]').click();
+
+    cy.get('.login-form > form > p', { timeout: 10000 })
+      .should('contain', 'Your email or password');
+  });
+
+  it('Logout de Usuário', () => {
+    cy.get('a[href="/login"]').click();
+    cy.get('[data-qa="login-email"]').type(userEmail);
+    cy.get('[data-qa="login-password"]').type('12345');
+    cy.get('[data-qa="login-button"]').click();
+
+    cy.contains(`Logged in as ${userName}`, { timeout: 10000 }).should('be.visible');
+
+    cy.contains('Logout', { timeout: 10000 }).should('be.visible').click();
+    cy.url({ timeout: 10000 }).should('contain', 'login');
+
+    cy.get('a[href="/logout"]').should('not.exist');
+    cy.get('a[href="/login"]').should('contain.text', 'Signup / Login');
+  });
+
+  it('Cadastrar Usuário com e-mail existente', () => {
+    cy.get('a[href="/login"]').click();
+    cy.get('[data-qa="signup-name"]').type(userName);
+    cy.get('[data-qa="signup-email"]').type(userEmail);
+    cy.contains('button', 'Signup').click();
+
+    cy.get('.signup-form p', { timeout: 10000 })
+      .should('be.visible')
+      .and('contain.text', 'Email Address already exist!');
+  });
+
+  it('Enviar formulário de contato com upload', () => {
+    cy.get('a[href*=contact]').click();
+    cy.get('[data-qa="name"]').type(userData.name);
+    cy.get('[data-qa="email"]').type(userData.email);
+    cy.get('[data-qa="subject"]').type(userData.subject);
+    cy.get('[data-qa="message"]').type(userData.message);
+
+    cy.fixture('userData.json').as('arquivo');
+    cy.get('input[type="file"]').selectFile('@arquivo');
+
+    cy.get('[data-qa="submit-button"]').click();
+    cy.get('.status', { timeout: 10000 })
+      .should('contain.text', 'Success! Your details have been submitted successfully.');
+  });
+
+  it('Verificar todos os produtos e detalhes do produto', () => {
+    cy.get('a[href="/products"]').click();
+    cy.url().should('include', '/products');
+    cy.get('.title.text-center').should('contain.text', 'All Products');
+    cy.get('.features_items .col-sm-4').should('have.length.greaterThan', 0);
+
+    cy.get('.features_items .col-sm-4').first().contains('View Product').click();
+    cy.url().should('include', '/product_details/');
+    cy.get('.product-information').should('be.visible');
+    cy.get('.product-information h2').should('not.be.empty');
+    cy.get('.product-information p').eq(0).should('contain.text', 'Category');
+    cy.get('.product-information p').eq(1).should('contain.text', 'Availability');
+    cy.get('.product-information p').eq(2).should('contain.text', 'Condition');
+    cy.get('.product-information p').eq(3).should('contain.text', 'Brand');
+  });
+
+  it('Pesquisar produto', () => {
+    cy.acessarPaginaProdutos(); 
+    cy.get('.features_items .col-sm-4').should('have.length.greaterThan', 0);
+    cy.get('.features_items .col-sm-4').each(($produto) => {
+      cy.wrap($produto).should('be.visible');
+      cy.wrap($produto).find('p').should('not.be.empty');
     });
+  });
+
+  it('Inscrever usuário na subscription', () => {
+    cy.get('footer').scrollIntoView();
+    cy.get('#susbscribe_email').type(userEmail);
+    cy.get('#subscribe').click();
+    cy.get('.alert-success')
+      .should('contain.text', 'You have been successfully subscribed!');
+  });
+
+ it('Fluxo completo de compra', () => {
+  cy.get('a[href="/login"]').click();
+
+
+  const fluxoUserEmail = `fluxo-${new Date().getTime()}@test.com`;
+  cy.get('[data-qa="signup-name"]').type(userName);
+  cy.get('[data-qa="signup-email"]').type(fluxoUserEmail);
+  cy.contains('button', 'Signup').click();
+
+  cy.get('[data-qa="password"]').type('12345');
+  cy.get('select[data-qa=days]').select('20');
+  cy.get('select[data-qa=months]').select('September');
+  cy.get('select[data-qa=years]').select('1992');
+
+  cy.get('input#newsletter').check();
+  cy.get('input#optin').check();
+  cy.get('input#first_name').type('Breno');
+  cy.get('input#last_name').type('Bruno');
+  cy.get('input#company').type('AMZ');
+  cy.get('input#address1').type('RUA 1');
+  cy.get('select#country').select('Canada');
+  cy.get('input#state').type('California');
+  cy.get('input#city').type('Los Angeles');
+  cy.get('[data-qa=zipcode]').type('90001');
+  cy.get('[data-qa=mobile_number]').type('222 333 4444');
+
+  cy.get('[data-qa="create-account"]').click();
+
+  cy.contains('b', 'Account Created!', { timeout: 10000 }).should('be.visible');
+  cy.get('a[data-qa="continue-button"]').click();
+
+  cy.contains(`Logged in as ${userName}`, { timeout: 10000 }).should('be.visible');
+
+  cy.adicionarPrimeiroProdutoCarrinho();
+  cy.get('.cart_info', { timeout: 10000 }).should('be.visible');
+
+  cy.finalizarCompra('Favor entregar rápido', {
+    nome: 'Breno Bruno',
+    numero: '4242424242424242',
+    cvc: '123',
+    mes: '12',
+    ano: '2025'
+  });
+
+  cy.contains('Congratulations! Your order has been confirmed!', { timeout: 10000 }).should('be.visible');
+
+  cy.get('a[href="/delete_account"]').click();
+  cy.get('b').should('contain.text', 'Account Deleted!');
+  cy.contains('Your account has been permanently deleted!').should('be.visible');
+  cy.get('a[data-qa="continue-button"], a:contains("Continue")').click();
+});
+
 });
--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -23,3 +23,142 @@
 //
 // -- This will overwrite an existing command --
 // Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
+
+//cy.
+
+Cypress.Commands.add('navegarParaLogin', () => {
+  cy.get('a[href="/login"]').click()
+});
+
+// 🔹 Acessar a página "Todos os Produtos" e validar o carregamento
+Cypress.Commands.add('acessarPaginaProdutos', () => {
+  cy.get('a[href="/products"]').click();
+  cy.url().should('include', '/products');
+  cy.get('.title.text-center')
+    .should('be.visible')
+    .and('contain.text', 'All Products');
+  cy.get('.features_items .col-sm-4')
+    .should('have.length.greaterThan', 0);
+});
+
+// 🔹 Pesquisar um produto e validar o resultado
+Cypress.Commands.add('pesquisarProduto', (nomeProduto) => {
+  cy.get('#search_product').type(nomeProduto);
+  cy.get('#submit_search').click();
+
+  // Verifica se seção "SEARCHED PRODUCTS" está visível
+  cy.get('.title.text-center')
+    .should('be.visible')
+    .and('contain.text', 'Searched Products');
+
+  // Garante que há produtos listados
+  cy.get('.features_items .col-sm-4')
+    .should('have.length.greaterThan', 0);
+});
+// Comando para assinar newsletter
+Cypress.Commands.add('assinarNewsletter', (email) => {
+  cy.get('footer').scrollIntoView();              // rola até o rodapé
+  cy.get('.single-widget h2').should('contain.text', 'SUBSCRIPTION'); // verifica título
+  cy.get('#susbscribe_email').type(email);        // digita o e-mail
+  cy.get('#subscribe').click();                   // clica no botão
+  cy.get('.alert-success')
+    .should('be.visible')
+    .and('contain.text', 'You have been successfully subscribed!');
+});
+// Comando para acessar a página de login
+Cypress.Commands.add('acessarLogin', () => {
+  cy.visit('https://automationexercise.com/');
+  cy.get('a[href="/login"]').click();
+});
+
+// Comando para cadastrar usuário
+Cypress.Commands.add('cadastrarUsuario', (userName, userEmail) => {
+  cy.get('[data-qa="signup-name"]').type(userName);
+  cy.get('[data-qa="signup-email"]').type(userEmail);
+  cy.contains('button', 'Signup').click();
+
+  cy.get('[data-qa="password"]').type('12345');
+  cy.get('select[data-qa=days]').select('20');
+  cy.get('select[data-qa=months]').select('September');
+  cy.get('select[data-qa=years]').select('1992');
+  cy.get('input#newsletter').check();
+  cy.get('input#optin').check();
+  cy.get('input#first_name').type('Breno');
+  cy.get('input#last_name').type('Bruno');
+  cy.get('input#company').type('AMZ');
+  cy.get('input#address1').type('RUA 1');
+  cy.get('select#country').select('Canada');
+  cy.get('input#state').type('California');
+  cy.get('input#city').type('Los Angeles');
+  cy.get('[data-qa=zipcode]').type('90001');
+  cy.get('[data-qa=mobile_number]').type('222 333 4444');
+
+  cy.get('[data-qa="create-account"]').click();
+});
+
+// Comando para login de usuário
+Cypress.Commands.add('loginUsuario', (userEmail, password = '12345') => {
+  cy.acessarLogin();
+  cy.get('[data-qa="login-email"]').type(userEmail);
+  cy.get('[data-qa="login-password"]').type(password);
+  cy.get('[data-qa="login-button"]').click();
+});
+
+// Comando para adicionar o primeiro produto ao carrinho
+Cypress.Commands.add('adicionarPrimeiroProdutoCarrinho', () => {
+  // Acessa a página de produtos, se ainda não estiver nela
+  cy.get('a[href="/products"]').click();
+
+  // Espera os produtos carregarem
+  cy.get('.features_items .col-sm-4', { timeout: 10000 }).should('have.length.greaterThan', 0);
+
+  // Clica em "Add to cart" do primeiro produto
+  cy.get('.features_items .col-sm-4').first().contains('Add to cart').click();
+
+  // Espera o modal de confirmação aparecer
+  cy.get('#cartModal', { timeout: 10000 }).should('be.visible');
+
+  // Clica em "View Cart" no modal
+  cy.get('#cartModal').contains('View Cart').click();
+
+  // Verifica se a página de carrinho carregou
+  cy.get('.cart_info', { timeout: 10000 }).should('be.visible');
+});
+
+// Comando para finalizar compra
+Cypress.Commands.add('finalizarCompra', (comentario, pagamento) => {
+  // Avança para checkout
+  cy.contains('Proceed To Checkout', { timeout: 10000 }).click();
+
+  // Digita comentário
+  cy.get('textarea[name="message"]').type(comentario);
+
+  // Coloca o pedido
+  cy.contains('Place Order').click();
+
+  // Preenche dados de pagamento
+  cy.get('input[name="name_on_card"]').type(pagamento.nome);
+  cy.get('input[name="card_number"]').type(pagamento.numero);
+  cy.get('input[name="cvc"]').type(pagamento.cvc);
+  cy.get('input[name="expiry_month"]').type(pagamento.mes);
+  cy.get('input[name="expiry_year"]').type(pagamento.ano);
+
+  // Clica no botão de pagamento
+  cy.get('button[data-qa="pay-button"]')
+    .scrollIntoView()
+    .should('be.visible')
+    .click();
+
+  // Espera que a mensagem de sucesso apareça na nova página
+  cy.contains('Congratulations! Your order has been confirmed!', { timeout: 10000 })
+    .should('be.visible');
+});
+
+
+
+// Comando para deletar conta
+Cypress.Commands.add('deletarConta', () => {
+  cy.get('a[href="/delete_account"]').click();
+  cy.contains('b', 'Account Deleted!').should('be.visible');
+  cy.get('a[data-qa="continue-button"]').click();
+});
