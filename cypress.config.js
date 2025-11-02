f69987694c2509bfe9bcc66c1be6c860558cb9f1
Bruna Lara <brunalara28@hotmail.com>
2025-11-02 17:03:17 -0300

    Trabalho Cypress Final

--- cypress.config.js
+++ cypress.config.js
@@ -2,6 +2,13 @@ const { defineConfig } = require('cypress');
 
 module.exports = defineConfig({
   reporter: 'cypress-mochawesome-reporter',
+  reporterOptions: {
+    reportDir: 'cypress/reports', 
+    charts: true,
+    reportPageTitle: 'Relatório de Testes',
+    embeddedScreenshots: true,
+    inlineAssets: true,
+  },
   e2e: {
     setupNodeEvents(on, config) {
       require('cypress-mochawesome-reporter/plugin')(on);
